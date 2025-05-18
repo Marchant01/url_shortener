7b6c3d031c4438f1c9fe961185d7e127dd78d218
Marchant01 <celvin.marchant@hotmail.com>
2025-05-18 21:32:02 +0200

    Implemented error handling and ui messages based on response.

--- url_shortener.py
+++ url_shortener.py
@@ -1,3 +1,4 @@
+import pyshorteners.exceptions
 import streamlit as st
 import pyshorteners
 
@@ -5,28 +6,34 @@ s = pyshorteners.Shortener()
 
 st.header('URL Shortener')
 
-if "url_output" not in st.session_state:
+# Initialize session state variables
+if 'url_output' not in st.session_state:
     st.session_state.url_output = ''
 
-def shorten_url(url: str) -> str:
+if 'url_input' not in st.session_state:
+    st.session_state.url_input = ''
+
+# Text input for URL
+url_input = st.text_input(label='Enter URL here:', 
+                          value='', 
+                          placeholder='http://example.url.com',
+                          key='url_input')
+
+# Shorten URL button
+shorten_pressed = st.button('Shorten')
+
+# Update session state if button is pressed
+if shorten_pressed:
+    # Try to shorten URL, changes the url_output session state and handles exceptions
+    if st.session_state.url_input == '':
+        st.text('Please enter a URL!')    
     try:
-        short_url = s.tinyurl.short(url)
-        return short_url
-    except Exception as e:
-        return f'Error: {str(e)}'
+        st.session_state.url_output = s.tinyurl.short(st.session_state.url_input)
+    except pyshorteners.exceptions.ShorteningErrorException:
+        st.text('Bad URL, try another!')
+    # The following usually occurs when an empty string is given
+    except pyshorteners.exceptions.BadURLException:
+        pass
 
-def change_url_text(url):
-    st.session_state
-
-url_input = st.text_input(label='URL:', value='')
-
-# Initiate a variable with a sessionstate
-url_output = st.session_state.url_output
-st.code(url_output)
-
-if st.session_state.get('shorten'):
-    st.session_state.url_output = shorten_url(url_input)
-    st.rerun()
-
-st.button('Shorten', key='shorten')
-# url_output = st.text_input(label='Shortened URL: ', disabled=True, placeholder='Short URL', value=st.session_state.url_output)
+# Code block for URL output
+st.code(st.session_state.url_output)
