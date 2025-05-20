598fe7267e518b77ab9a4a665da96121a1b26430
Marchant01 <celvin.marchant@hotmail.com>
2025-05-20 12:39:06 +0200

    Implemented qr code generator and a image element to display it

--- url_shortener.py
+++ url_shortener.py
@@ -1,6 +1,8 @@
 import pyshorteners.exceptions
 import streamlit as st
 import pyshorteners
+import qrcode
+from io import BytesIO
 
 s = pyshorteners.Shortener()
 
@@ -12,6 +14,9 @@ if 'url_output' not in st.session_state:
 
 if 'url_input' not in st.session_state:
     st.session_state.url_input = ''
+
+if 'qr_code_img' not in st.session_state:
+    st.session_state.qr_code_img = ''
 
 # Text input for URL
 url_input = st.text_input(label='Enter URL here:', 
@@ -29,11 +34,22 @@ if shorten_pressed:
         st.text('Please enter a URL!')    
     try:
         st.session_state.url_output = s.tinyurl.short(st.session_state.url_input)
+        # Generate qr code from the shortened url
+        qr_image = qrcode.make(st.session_state.url_output)
+        img_bytes = BytesIO()
+        # Store the image as a png
+        qr_image.save(img_bytes, format='PNG')
+        img_bytes.seek(0)
+        # Set the session state of the image widget
+        st.session_state.qr_code_img = img_bytes
     except pyshorteners.exceptions.ShorteningErrorException:
         st.text('Bad URL, try another!')
     # The following usually occurs when an empty string is given
     except pyshorteners.exceptions.BadURLException:
         pass
 
-# Code block for URL output
-st.code(st.session_state.url_output)
+# Display code block and image for URL output when url is generated
+if st.session_state.url_output:
+    st.code(st.session_state.url_output)
+    st.image(st.session_state.qr_code_img, 
+            caption=st.session_state.url_output)
